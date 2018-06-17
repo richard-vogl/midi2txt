f759777dd74fa7e45bc162c456dc19dadfb26e02
Richard Vogl <richard.vogl@gmx.at>
2018-06-17 14:08:25 +0200

    refactor main and midi2txt for external use

--- midi2txt/midi_to_txt.py
+++ midi2txt/midi_to_txt.py
@@ -6,24 +6,9 @@ import os
 import copy
 
 
-def midi_to_txt(input_file, output_file=None, offset=0, bpm=120, write_beats=False, beats_file=None):
-
-    in_file_path = os.path.dirname(input_file)
-    in_file_name = os.path.basename(input_file)
-    file_name_wo_ext, _ = os.path.splitext(in_file_name)
-
-    if output_file is None:
-        output_file = os.path.join(in_file_path, file_name_wo_ext + ".txt")
-
-    out_file_path = os.path.dirname(output_file)
-    out_file_name = os.path.basename(output_file)
-    out_file_name_wo_ext, _ = os.path.splitext(out_file_name)
-
-    if beats_file is None:
-        beats_file = os.path.join(out_file_path, out_file_name_wo_ext + ".beats")
+def midi_to_txt(input_file, bpm=120, calc_beats=False):
 
     times = []
-
     max_time = 0
 
     infile = MidiFile(input_file)
@@ -31,8 +16,6 @@ def midi_to_txt(input_file, output_file=None, offset=0, bpm=120, write_beats=Fal
 
     midi_tempo = bpm2tempo(bpm)
     s_per_tick = midi_tempo / 1000.0 / 1000 / ppq
-
-    print("Reading midi file '"+input_file+"' ...")
 
     file_type = infile.type
 
@@ -87,7 +70,29 @@ def midi_to_txt(input_file, output_file=None, offset=0, bpm=120, write_beats=Fal
                 velocity = float(message.velocity) / 127.0
                 times.append([cur_time, inst_idx, velocity])
 
-    print("Writing output ...")
+    if calc_beats:
+        beat_times = calc_beat_times(copy.deepcopy(infile.tracks[0]), max_time, ppq)
+    else:
+        beat_times = None
+
+    return times, beat_times
+
+
+def write_output(times, beat_times, output_file, beats_file=None, write_beats=False, offset=0, input_file=None):
+
+    if output_file is None:
+        in_file_path = os.path.dirname(input_file)
+        in_file_name = os.path.basename(input_file)
+        file_name_wo_ext, _ = os.path.splitext(in_file_name)
+        output_file = os.path.join(in_file_path, file_name_wo_ext + ".txt")
+
+    out_file_path = os.path.dirname(output_file)
+    out_file_name = os.path.basename(output_file)
+    out_file_name_wo_ext, _ = os.path.splitext(out_file_name)
+
+    if beats_file is None:
+        beats_file = os.path.join(out_file_path, out_file_name_wo_ext + ".beats")
+
     # sort by time (for multiple tracks)
     times.sort(key=lambda tup: tup[0])
     with open(output_file, 'w') as f:
@@ -95,29 +100,38 @@ def midi_to_txt(input_file, output_file=None, offset=0, bpm=120, write_beats=Fal
             f.write("%3.5f \t %d \t %1.3f \n" % (entry[0]+offset, entry[1], entry[2]))
 
     if write_beats:
-        beat_times = calc_beat_times(copy.deepcopy(infile.tracks[0]), max_time, ppq)
         with open(beats_file, 'w') as f:
             for entry in beat_times:
                 f.write("%3.5f \t %d\n" % (entry[0]+offset, entry[1]))
 
-    print("Finished.")
 
-
-if __name__ == '__main__':
-
+def main():
     # add argument parser
     parser = argparse.ArgumentParser(
         description='Convert midi annotations for drum files to txt.')
     parser.add_argument('--infile', '-i', help='input audio file.')
     parser.add_argument('--outfile', '-o', help='output file name.', default=None)
     parser.add_argument('--time_offset', '-m', help='offset for time of labels.', default=0, type=float)
-    parser.add_argument('--tempo', '-t', help='Tempo to be used (in BPM) if MIDI file doesn\'t contain tempo events.', default=120, type=float)
+    parser.add_argument('--tempo', '-t', help='Tempo to be used (in BPM) if MIDI file doesn\'t contain tempo events.',
+                        default=120, type=float)
+    parser.add_argument('--beatsout', '-b', help='Write beats files.', action='store_true')
 
     args = parser.parse_args()
-
     input_file = args.infile
     output_file = args.outfile
     offset = args.time_offset
     bpm_param = args.tempo
+    write_beats = args.beatsout
 
-    midi_to_txt(input_file, output_file, offset, bpm_param, False, None)
+    print("Reading midi file '" + input_file + "' ...")
+    times, beat_times = midi_to_txt(input_file, bpm_param, write_beats)
+    print("Writing output ...")
+    write_output(times, beat_times, output_file, beats_file=None, write_beats=write_beats,
+                 offset=offset, input_file=input_file)
+    print("Finished.")
+
+
+if __name__ == '__main__':
+    main()
+
+
